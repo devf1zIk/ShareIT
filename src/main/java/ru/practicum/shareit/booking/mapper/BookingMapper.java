7224ed145482c92312e0b792ff98d43e238976f1
daniyardoskulov <daniyar.doskulov@narxoz.kz>
2025-07-13 19:24:41 +0500

    Добавлен UtilityClass

--- src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
@@ -1,8 +1,10 @@
 package ru.practicum.shareit.booking.mapper;
 
+import lombok.experimental.UtilityClass;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.model.Booking;
 
+@UtilityClass
 public class BookingMapper {
 
     public static BookingDto toDto(Booking booking) {
