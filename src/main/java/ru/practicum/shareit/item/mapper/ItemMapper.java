eec27bc35e146b64c753351a426995f888b5fa1a
daniyardoskulov <daniyar.doskulov@narxoz.kz>
2025-07-15 03:12:20 +0500

    проверка mapper

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -7,7 +7,14 @@ import ru.practicum.shareit.item.model.Item;
 @Mapper(componentModel = "spring")
 public interface ItemMapper {
 
+    @Mapping(source = "owner.id", target = "ownerId")
+    @Mapping(target = "request", ignore = true)
+    @Mapping(target = "lastBooking", ignore = true)
+    @Mapping(target = "nextBooking", ignore = true)
+    @Mapping(target = "comments", ignore = true)
     ItemDto toDto(Item item);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "owner", ignore = true)
     Item toEntity(ItemDto itemDto);
 }
