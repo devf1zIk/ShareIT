dd47ea511c230b40df1c3df61462dee06fe26160
daniyardoskulov <daniyar.doskulov@narxoz.kz>
2025-07-15 13:04:10 +0500

    fix

--- src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
@@ -13,15 +13,23 @@ import java.util.List;
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
     List<Booking> findByBookerId(Long bookerId, Sort sort);
+
     List<Booking> findByBookerIdAndStartBeforeAndEndAfter(Long bookerId, LocalDateTime start, LocalDateTime end, Sort sort);
+
     List<Booking> findByBookerIdAndEndBefore(Long bookerId, LocalDateTime end, Sort sort);
+
     List<Booking> findByBookerIdAndStartAfter(Long bookerId, LocalDateTime start, Sort sort);
+
     List<Booking> findByBookerIdAndStatus(Long bookerId, BookingStatus status, Sort sort);
 
     List<Booking> findByItem_Owner_Id(Long ownerId, Sort sort);
+
     List<Booking> findByItem_Owner_IdAndStartBeforeAndEndAfter(Long ownerId, LocalDateTime start, LocalDateTime end, Sort sort);
+
     List<Booking> findByItem_Owner_IdAndEndBefore(Long ownerId, LocalDateTime end, Sort sort);
+
     List<Booking> findByItem_Owner_IdAndStartAfter(Long ownerId, LocalDateTime start, Sort sort);
+
     List<Booking> findByItem_Owner_IdAndStatus(Long ownerId, BookingStatus status, Sort sort);
 
     @Query("SELECT b FROM Booking b " +
